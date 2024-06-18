384747ee3fd31b8768940bb52a5a8a993fd9025f
Urk Bio <114656103+urkbio@users.noreply.github.com>
2024-06-18 21:52:37 +0800

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,5 +1,13 @@
 rm ./adgh.txt
 wget https://ghproxy.net/https://raw.githubusercontent.com/urkbio/AdGuard-Rule/main/rule/adgh.txt
-wget https://raw.hellogithub.com/hosts
+
+# 拉取加速规则，以下两个任选，更多请看源项目首页
+# Github520 https://github.com/521xueweihan/GitHub520
+#wget https://raw.hellogithub.com/hosts
+# SteamHostSync https://github.com/Clov614/SteamHostSync
+wget https://raw.sevencdn.com/Clov614/SteamHostSync/main/Hosts
+
+# 从合并的黑名单中剔除关键字匹配的内容
 bash filterlite.sh
+# 将host格式转为adgh支持的rewrite格式
 bash host2adgh.sh
