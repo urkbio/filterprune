4bdcc32d5072a73087f65a0921c3923c23e7d57d
Urk Bio <114656103+urkbio@users.noreply.github.com>
2024-07-05 10:41:56 +0800

    更新 keepalive.sh

--- keepalive.sh
+++ keepalive.sh
@@ -9,7 +9,7 @@ if systemctl is-active --quiet $SERVICE_NAME; then
     echo "$SERVICE_NAME is running."
 else
     echo "$SERVICE_NAME is not running. Restarting..."
-    sudo systemctl restart $SERVICE_NAME
+    systemctl restart $SERVICE_NAME
     
     # 检查服务是否重新启动成功
     if systemctl is-active --quiet $SERVICE_NAME; then
